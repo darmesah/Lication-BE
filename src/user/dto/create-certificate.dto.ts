fc08dc13882498d4f4dbfcac9b098442133705e3
Damisa Alex <damisa.alex@gmail.com>
2024-05-02 04:12:39 +0100

    update

--- src/user/dto/create-certificate.dto.ts
+++ src/user/dto/create-certificate.dto.ts
@@ -2,127 +2,126 @@ import { ApiProperty } from '@nestjs/swagger';
 import { IsString, IsOptional, IsNotEmpty } from 'class-validator';
 
 export class CreateCertificateDTO {
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   firstName: string;
 
   @ApiProperty({ required: false })
-  @IsString()
-  @IsNotEmpty()
+  @IsOptional()
   middleName: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   lastName: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   dateOfBirth: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   stateOfOrigin: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   lgaOfOrigin: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   email: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   phone: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   address: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   state: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   lga: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   passportNumber: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   dateOfIssue: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   dateOfExpiry: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   placeOfIssue: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   purposeOfTravel: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   destinationCountry: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   durationOfStay: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   addressInDestination: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   currentOccupation: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   employerName: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   employerAddress: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   position: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   preferredAppointmentTime: string;
 
-  @ApiProperty({ required: false })
+  @ApiProperty({ required: true })
   @IsString()
   @IsNotEmpty()
   nearestCaptureCenter: string;
